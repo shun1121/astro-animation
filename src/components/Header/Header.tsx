1ef218a5c5277d15ee5581b9c35de863c7aafc11
shun1121 <hikitanikun.ware80000@gmail.com>
2023-07-07 16:52:23 +0900

    :sparkles: Set an path alias and add header component
    set an path alias and add header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="flex justify-end m-[12px]">
+      <button className="border-2 border-[#4A4F54] text-[#4A4F54] rounded-full w-[80px] h-[80px]">
+        <div className="">
+          MENU
+        </div>
+      </button>
+    </header>
+  )
+}
