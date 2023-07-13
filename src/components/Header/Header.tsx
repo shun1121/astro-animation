29ffd5431c344e001acfc87a2d26fdce8e3be85b
shun1121 <hikitanikun.ware80000@gmail.com>
2023-07-13 18:01:48 +0900

    :sparkles: Gsap導入、カウントアップアニメーション実装、Header.tsx途中
    gsap導入、カウントアップアニメーション実装、Header.tsx途中

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,24 @@
+import { gsap } from 'gsap';
+import { useEffect, useRef } from 'react';
+
 export const Header = () => {
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  useEffect(() => {
+    const handleMouseEnter = () => {
+      gsap.to(buttonRef.current, {
+        x: 5,
+        duration: 0.3,
+      });
+    };
+
+    if (buttonRef.current) {
+      buttonRef.current.addEventListener('mouseenter', handleMouseEnter);
+    }
+  },[])
+
   return (
-    <header className="flex justify-end m-[12px]">
-      <button className="border-2 border-[#4A4F54] text-[#4A4F54] rounded-full w-[80px] h-[80px]">
+    <header className="flex justify-end m-[12px] bg-gray-300">
+      <button className="border-2 border-[#4A4F54] text-[#4A4F54] rounded-full w-[80px] h-[80px]" ref={buttonRef}>
         <div className="">
           MENU
         </div>
